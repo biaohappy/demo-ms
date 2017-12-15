22aaad5b94af6f65a66ba3fba664d21b7a96430c
wuxiaobiao <514774517@qq.com>
2017-12-15 16:41:11 +0800

    12

--- demo-common/src/main/java/com/xiaobiao/exception/DemoErrorCode.java
+++ demo-common/src/main/java/com/xiaobiao/exception/DemoErrorCode.java
@@ -10,10 +10,10 @@ public enum DemoErrorCode {
 
     ERROR_CODE_341FFF("341FFF", "程序异常"),
     ERROR_CODE_341001("341001", "亲，您查询的信息不存在哦"),
-    ERROR_CODE_341002("341002", "用户名错误"),
-    ERROR_CODE_341003("341003", "密码错误"),
-    ERROR_CODE_341004("341004", "认证失败"),
-    ERROR_CODE_341005("341005", "登录失败");
+    ERROR_CODE_341002("341002", "请输入正确的用户名!"),
+    ERROR_CODE_341003("341003", "请输入正确的密码!"),
+    ERROR_CODE_341004("341004", "*认证失败!"),
+    ERROR_CODE_341005("341005", "*登录失败!");
 
     @Getter
     private String errorCode;
